55ee409eb4b27e528408221bf35db09ea72115fc
hapowers42 <32717754+hapowers42@users.noreply.github.com>
2021-12-02 09:01:37 -0500

    day 2

---
+++ day2/day2.py
@@ -0,0 +1,19 @@
+from functools import reduce
+
+def add_command(a, b):
+	cmd = b.split()
+	if cmd[0] == "forward":
+		return (a[0] + int(cmd[1]), a[1])
+	elif cmd[0] == "down":
+		return (a[0], a[1] + int(cmd[1]))
+	else:
+		return (a[0], a[1] - int(cmd[1]))
+
+with open("input", "r") as f:
+	cmdList = f.readlines()
+
+	# (horiz, vert)
+	pos = reduce(add_command, cmdList, (0, 0))
+
+	print(pos)
+	print(pos[0] * pos[1])
